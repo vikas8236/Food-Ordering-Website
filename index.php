9373e0496c6375139e2a40ad7f2f0481b1e299f9
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-06-03 20:42:52 +0530

    Food Ordering Website
    it's a food-ordering website with limited features.
    I used Html CSS JavaScript for front-end development and PHP as a scripting language. I used MySQL as a database.

---
+++ index.php
@@ -0,0 +1,190 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>OnlineFood.com</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap"
+      rel="stylesheet"/>
+
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
+ <!-- <link rel="stylesheet" href="style.css">  -->
+<link rel="stylesheet" href="mara.css">
+    <link rel="stylesheet" href="script.js"> 
+    <link rel="stylesheet" media="screen and (max-width: 1100px)" href="mobile.css" />
+  </head>
+  <body>
+  <!-- This is our header section   -->
+    <header>
+      <!-- Code for Navigation bar -->
+    <nav id="navbar">
+      <div id="logo">
+        <img src="logo.png" alt="OnlineFood.com" />
+      </div>
+      <ul>
+        <li class="item"><a href="#home">Home</a></li>
+        <li class="item"><a href="#services-container">Services</a></li>
+        <li class="item"><a href="#client-section">About Us</a></li>
+        <li class="item"><a href="#contact">Contact Us</a></li>
+      </ul>
+      <div class="search-bar">
+        <input type="text" id="search-input" placeholder="Search for food..."required>
+        <button id="search-button" value ="search" >Search</button>
+    </div>
+    </nav>
+     
+  </header>
+
+    <!-- Code for Home section -->
+     <section id="home">
+        <h1 class="h-primary">Welcome to MyOnlineMeal</h1>
+        <p> Your go-to destination for delicious food delivered straight to your door! 
+
+        We believe that great food should be easy to access, no matter where you are. That's why we've created a platform that connects you with some of the best local restaurants in your area, so you can enjoy a meal from the comfort of your own home.
+
+        Our process is simple: browse our selection of restaurants, choose your favorite dishes, and place your order online. Our delivery partners will then pick up your food and bring it straight to your door, so you can enjoy a hot and fresh meal without ever leaving your house.
+
+        We're passionate about quality food and exceptional customer service, which is why we only partner with the best restaurants in your area. Whether you're in the mood for classic Italian dishes, spicy Asian cuisine, or a juicy burger and fries, we've got you covered.
+
+        So why wait? Browse our selection of restaurants today and start enjoying delicious food delivered right to your door. Thank you for choosing Foodie.com! </p>
+        <button class="btn"><a href="#contact">Order Now</a></button>
+    </section>
+    <!-- Code for services section -->
+    <section id="services-container">
+      <h1 class="h-primary center">Our Services</h1>
+      <div id="services">
+        <div class="box">
+          <img src="pizza1.png" alt="" />
+
+          <h2 class="h-secondary center">Food Catering</h2>
+          <p>
+            At our food delivery service, we're more than just a meal delivery service - we're also a full-service catering company. Whether you're planning a small gathering or a large event, we can provide delicious, high-quality food that will leave your guests impressed and satisfied.
+            
+            Our catering menu offers a wide variety of options to suit every taste and dietary need. From classic comfort foods to elegant gourmet dishes, we can create a custom menu that perfectly matches the style and theme of your event. Plus, we use only the freshest and highest-quality ingredients, so you can be sure that your guests are getting a healthy and delicious meal.
+            
+            We also offer a range of catering services to make your event planning as stress-free as possible. Our experienced catering team can help you plan every aspect of your event, from choosing the right menu to arranging the perfect setup and decor. We can also provide professional servers and bartenders to ensure that your guests are taken care of throughout the event.
+            
+            So why not let us take care of the catering for your next event? Whether you're planning a wedding, corporate event, or anything in between, we've got everything you need to make your event a success. Contact us today to learn more about our catering services and to start planning your custom menu.
+          </p>
+        </div>
+        <div class="box">
+          <img src="bulkfood.png" alt="" />
+
+          <h2 class="h-secondary center">Bulk Ordering</h2>
+          <p>
+            At our food delivery service, we believe that good food should be shared. That's why we offer bulk ordering options for customers who want to enjoy our delicious meals with their friends, family, or colleagues.
+            
+            Whether you're hosting a party, planning a work event, or just want to stock up on some tasty meals for the week ahead, our bulk ordering options make it easy to get exactly what you need. You can choose from a wide variety of dishes and customize your order to suit your preferences, so you can be sure that everyone will find something they love.
+            
+            Not only is bulk ordering a convenient way to feed a crowd, but it can also save you money. We offer discounts on bulk orders, so you can enjoy our delicious meals at a more affordable price. Plus, with our fast and reliable delivery service, you can be sure that your food will arrive on time and ready to serve.
+            
+            So why not take advantage of our bulk ordering options today? Whether you're planning a big event or simply want to stock up on some delicious meals for the week ahead, we've got everything you need to make your next meal a success.
+          </p>
+        </div>
+        <div class="box">
+          <img src="delivery.png" alt="" />
+
+          <h2 class="h-secondary center">Food Devivery</h2>
+          <p>
+            Welcome to our food delivery service! We offer a wide variety of delicious meals and snacks that can be delivered straight to your doorstep. Whether you're craving Italian, Mexican, Chinese, or something else entirely, we've got you covered.
+            
+            All of our meals are prepared fresh to order using only the finest ingredients, so you can be sure that you're getting a high-quality and healthy meal every time you order. We also offer a range of options for those with dietary restrictions or special needs, including vegetarian, vegan, gluten-free, and more.
+            
+            Ordering is quick and easy - simply browse our menu, select your items, and check out. You can choose to have your food delivered right away, or schedule a delivery time that works best for you. We offer fast and reliable delivery, so you can always count on your food arriving hot and fresh.
+            
+            So why not give us a try today? We're confident that you'll love our delicious food and convenient delivery service. Whether you're enjoying a meal with family, hosting a party, or simply looking for a quick and easy dinner option, we've got everything you need to satisfy your cravings.
+          </p>
+        </div>
+      </div>
+    </section>
+    <!-- Code for clients section -->
+    <section id="client-section">
+      <h1 class="h-primary center">Our Clients</h1>
+      <div id="clients">
+        <div class="client-item">
+          <img src="applelogo.png" alt="" />
+        </div>
+        <div class="client-item">
+          <img src="microsoftlogo.png" alt="" />
+        </div>
+        <div class="client-item">
+          <img src="amazon.png" alt="" />
+        </div>
+        <div class="client-item">
+          <img src="tcs.png" alt="" />
+        </div>
+        <div class="client-item">
+          <img src="infosys.png" alt="" />
+        </div>
+      </div>
+    </section>
+    <!--  Code for contact section -->
+    <section id="contact">
+      <h1 class="h-primary center">Contact Us</h1>
+      <div id="contact-box">
+      <form name="formcontact" action="" method="post">
+
+          <label for="name"></label>
+          <input type="text"name="name"id="name"placeholder="name"required />
+          <label for="email"></label>
+          <input type="email" name="email"id="email"placeholder="email"required />
+          <label for="MobileNo"></label>
+          <input  type="phone"name="phone"id="MobileNo"placeholder="phone" required />
+           
+          <label for="address"></label>
+          <input type="text" name="address" id="address" placeholder="address" required />
+
+          <label for="massage"></label>
+          <textarea name="massage"id="massage"cols="20" rows="5" placeholder="Write Short Description about Your order..."></textarea>
+           <input type="submit" id ="save"class ="submit"name="submit" value="Submit">
+        </form>
+      </div>
+    </section>
+    <hr>
+    <!-- footer of the website  -->
+    <footer>
+      <div class="center">
+        Copyright &copy; www.onlinefood.com All rights reserved!
+      </div>
+
+      <div class =" social">
+        <h3>Follow us on </h3>
+        <img src="insta.png" alt="Instagram">
+        <img src="facebook.png" alt="facebook">
+        <img src="youtube-g8412d3321_1920.png" alt="YouTube">
+      </div>
+    </footer>
+  </body>
+</html>
+<!-- php script for connecting database  -->
+<?php
+$con=mysqli_connect("localhost","root","","aks");
+//mysql_select_db();
+if(isset($_POST['submit']))
+{
+  
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$address=$_POST['address'];
+$massage=$_POST['massage'];
+mysqli_query($con,"insert into food values('$name','$email','$phone','$address','$massage')");
+echo'<script>alert("Data saved")</script>';
+
+}
+?>
---
+++ script.js
@@ -0,0 +1,25 @@
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+
+searchButton.addEventListener("click", function() {
+  const searchTerm = searchInput.value;
+  // Here you can add the code to search for the entered term
+});
